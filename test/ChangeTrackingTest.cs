6499f9624c5477fed857529f75def289657fc4f3
David <kooldave98@hotmail.com>
2015-08-21 08:58:31 +0100

    manual-change-tracking
    In the docs, there was a way to trigger binding from objects that
    didn’t implement [Automatic Change Tracking]. This has now been added.

--- test/ChangeTrackingTest.cs
+++ test/ChangeTrackingTest.cs
@@ -4,334 +4,364 @@ using System.ComponentModel;
 
 namespace Praeclarum.Bind.Test
 {
-	[TestFixture]
-	public class ChangeTrackingTest
-	{
-		[SetUp]
-		public void SetUp ()
-		{
-			Binding.Error += message => Console.WriteLine (message);
-		}
-
-		class PropertyChangedEventObject
-		{
-			string stringValue = "";
-
-			public int StringValueChangedCount { get; private set; }
-
-			EventHandler stringValueChanged; 
-			public event EventHandler StringValueChanged 
-			{ 
-				add {
-					stringValueChanged += value;
-					StringValueChangedCount++;
-				} 
-				remove {
-					stringValueChanged -= value;
-					StringValueChangedCount--;
-				}
-			} 
-
-			public string StringValue {
-				get { return stringValue; }
-				set {
-					if (stringValue != value) {
-						stringValue = value;
-						if (stringValueChanged != null) {
-							stringValueChanged (this, EventArgs.Empty);
-						}
-					}
-				}
-			}
-		}
+    [TestFixture]
+    public class ChangeTrackingTest
+    {
+        [SetUp]
+        public void SetUp ()
+        {
+            Binding.Error += message => Console.WriteLine (message);
+        }
+
+        class PropertyChangedEventObject
+        {
+            string stringValue = "";
+
+            public int StringValueChangedCount { get; private set; }
+
+            EventHandler stringValueChanged; 
+            public event EventHandler StringValueChanged 
+            { 
+                add {
+                    stringValueChanged += value;
+                    StringValueChangedCount++;
+                } 
+                remove {
+                    stringValueChanged -= value;
+                    StringValueChangedCount--;
+                }
+            } 
+
+            public string StringValue {
+                get { return stringValue; }
+                set {
+                    if (stringValue != value) {
+                        stringValue = value;
+                        if (stringValueChanged != null) {
+                            stringValueChanged (this, EventArgs.Empty);
+                        }
+                    }
+                }
+            }
+        }
 
-		[Test]
-		public void PropertyChanged ()
-		{
-			var obj = new PropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var left = "";
+        [Test]
+        public void PropertyChanged ()
+        {
+            var obj = new PropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var left = "";
 
-			Binding.Create (() => left == obj.StringValue);
+            Binding.Create (() => left == obj.StringValue);
 
-			Assert.AreEqual (1, obj.StringValueChangedCount);
+            Assert.AreEqual (1, obj.StringValueChangedCount);
 
-			Assert.AreEqual (obj.StringValue, left);
+            Assert.AreEqual (obj.StringValue, left);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual (obj.StringValue, left);
-		}
+            Assert.AreEqual (obj.StringValue, left);
+        }
 
-		[Test]
-		public void MultipleObjectPropertyChanged ()
-		{
-			var objA = new PropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var objB = new PropertyChangedEventObject {
-				StringValue = "World",
-			};
-			var left = "";
+        [Test]
+        public void MultipleObjectPropertyChanged ()
+        {
+            var objA = new PropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var objB = new PropertyChangedEventObject {
+                StringValue = "World",
+            };
+            var left = "";
 
-			Binding.Create (() => left == objA.StringValue + ", " + objB.StringValue);
+            Binding.Create (() => left == objA.StringValue + ", " + objB.StringValue);
 
-			Assert.AreEqual ("Hello, World", left);
+            Assert.AreEqual ("Hello, World", left);
 
-			objA.StringValue = "Goodbye";
+            objA.StringValue = "Goodbye";
 
-			Assert.AreEqual ("Goodbye, World", left);
+            Assert.AreEqual ("Goodbye, World", left);
 
-			objB.StringValue = "Mars";
+            objB.StringValue = "Mars";
 
-			Assert.AreEqual ("Goodbye, Mars", left);
+            Assert.AreEqual ("Goodbye, Mars", left);
 
-		}
+        }
 
-		[Test]
-		public void MultiplePropertyChanged ()
-		{
-			var obj = new PropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var leftA = "";
-			var leftB = "";
+        [Test]
+        public void MultiplePropertyChanged ()
+        {
+            var obj = new PropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var leftA = "";
+            var leftB = "";
 
-			Binding.Create (() => leftA == obj.StringValue);
-			Binding.Create (() => leftB == obj.StringValue + "...");
+            Binding.Create (() => leftA == obj.StringValue);
+            Binding.Create (() => leftB == obj.StringValue + "...");
 
-			Assert.AreEqual (1, obj.StringValueChangedCount);
+            Assert.AreEqual (1, obj.StringValueChangedCount);
 
-			Assert.AreEqual ("Hello", leftA);
-			Assert.AreEqual ("Hello...", leftB);
+            Assert.AreEqual ("Hello", leftA);
+            Assert.AreEqual ("Hello...", leftB);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual ("Goodbye", leftA);
-			Assert.AreEqual ("Goodbye...", leftB);
-		}
+            Assert.AreEqual ("Goodbye", leftA);
+            Assert.AreEqual ("Goodbye...", leftB);
+        }
 
-		[Test]
-		public void RemoveMultiplePropertyChanged ()
-		{
-			var obj = new PropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var leftA = "";
-			var leftB = "";
+        [Test]
+        public void RemoveMultiplePropertyChanged ()
+        {
+            var obj = new PropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var leftA = "";
+            var leftB = "";
 
-			var bA = Binding.Create (() => leftA == obj.StringValue);
-			var bB = Binding.Create (() => leftB == obj.StringValue + "...");
+            var bA = Binding.Create (() => leftA == obj.StringValue);
+            var bB = Binding.Create (() => leftB == obj.StringValue + "...");
 
-			Assert.AreEqual (1, obj.StringValueChangedCount);
+            Assert.AreEqual (1, obj.StringValueChangedCount);
 
-			Assert.AreEqual ("Hello", leftA);
-			Assert.AreEqual ("Hello...", leftB);
+            Assert.AreEqual ("Hello", leftA);
+            Assert.AreEqual ("Hello...", leftB);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual ("Goodbye", leftA);
-			Assert.AreEqual ("Goodbye...", leftB);
+            Assert.AreEqual ("Goodbye", leftA);
+            Assert.AreEqual ("Goodbye...", leftB);
 
-			bA.Unbind ();
+            bA.Unbind ();
 
-			Assert.AreEqual (1, obj.StringValueChangedCount);
+            Assert.AreEqual (1, obj.StringValueChangedCount);
 
-			obj.StringValue = "Hello Again";
+            obj.StringValue = "Hello Again";
 
-			Assert.AreEqual ("Goodbye", leftA);
-			Assert.AreEqual ("Hello Again...", leftB);
+            Assert.AreEqual ("Goodbye", leftA);
+            Assert.AreEqual ("Hello Again...", leftB);
 
-			bB.Unbind ();
+            bB.Unbind ();
 
-			Assert.AreEqual (0, obj.StringValueChangedCount);
+            Assert.AreEqual (0, obj.StringValueChangedCount);
 
-			obj.StringValue = "Goodbye Again";
+            obj.StringValue = "Goodbye Again";
 
-			Assert.AreEqual ("Goodbye", leftA);
-			Assert.AreEqual ("Hello Again...", leftB);
-		}
+            Assert.AreEqual ("Goodbye", leftA);
+            Assert.AreEqual ("Hello Again...", leftB);
+        }
 
 
-		[Test]
-		public void RemovePropertyChanged ()
-		{
-			var obj = new PropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var left = "";
+        [Test]
+        public void RemovePropertyChanged ()
+        {
+            var obj = new PropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var left = "";
 
-			var b = Binding.Create (() => left == obj.StringValue);
+            var b = Binding.Create (() => left == obj.StringValue);
 
-			Assert.AreEqual (1, obj.StringValueChangedCount);
+            Assert.AreEqual (1, obj.StringValueChangedCount);
 
-			Assert.AreEqual (obj.StringValue, left);
+            Assert.AreEqual (obj.StringValue, left);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual (obj.StringValue, left);
+            Assert.AreEqual (obj.StringValue, left);
 
-			b.Unbind ();
+            b.Unbind ();
 
-			Assert.AreEqual (0, obj.StringValueChangedCount);
+            Assert.AreEqual (0, obj.StringValueChangedCount);
 
-			obj.StringValue = "Hello Again";
+            obj.StringValue = "Hello Again";
 
-			Assert.AreEqual ("Goodbye", left);
-		}
+            Assert.AreEqual ("Goodbye", left);
+        }
 
-		class NotifyPropertyChangedEventObject : INotifyPropertyChanged
-		{
-			public int PropertyChangedCount { get; private set; }
+        class NotifyPropertyChangedEventObject : INotifyPropertyChanged
+        {
+            public int PropertyChangedCount { get; private set; }
 
-			PropertyChangedEventHandler propertyChanged; 
-			public event PropertyChangedEventHandler PropertyChanged 
-			{ 
-				add {
-					propertyChanged += value;
-					PropertyChangedCount++;
-				} 
-				remove {
-					propertyChanged -= value;
-					PropertyChangedCount--;
-				}
-			} 
+            PropertyChangedEventHandler propertyChanged; 
+            public event PropertyChangedEventHandler PropertyChanged 
+            { 
+                add {
+                    propertyChanged += value;
+                    PropertyChangedCount++;
+                } 
+                remove {
+                    propertyChanged -= value;
+                    PropertyChangedCount--;
+                }
+            } 
 
-			string stringValue = "";
+            string stringValue = "";
 
-			public string StringValue {
-				get { return stringValue; }
-				set {
-					if (stringValue != value) {
-						stringValue = value;
-						if (propertyChanged != null) {
-							propertyChanged (this, new PropertyChangedEventArgs ("StringValue"));
-						}
-					}
-				}
-			}
+            public string StringValue {
+                get { return stringValue; }
+                set {
+                    if (stringValue != value) {
+                        stringValue = value;
+                        if (propertyChanged != null) {
+                            propertyChanged (this, new PropertyChangedEventArgs ("StringValue"));
+                        }
+                    }
+                }
+            }
 
-			int intValue = 0;
+            int intValue = 0;
 
-			public int IntValue {
-				get { return intValue; }
-				set {
-					if (intValue != value) {
-						intValue = value;
-						if (propertyChanged != null) {
-							propertyChanged (this, new PropertyChangedEventArgs ("IntValue"));
-						}
-					}
-				}
-			}
-		}
+            public int IntValue {
+                get { return intValue; }
+                set {
+                    if (intValue != value) {
+                        intValue = value;
+                        if (propertyChanged != null) {
+                            propertyChanged (this, new PropertyChangedEventArgs ("IntValue"));
+                        }
+                    }
+                }
+            }
+        }
 
-		[Test]
-		public void NotifyPropertyChanged ()
-		{
-			var obj = new NotifyPropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var left = "";
+        [Test]
+        public void NotifyPropertyChanged ()
+        {
+            var obj = new NotifyPropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var left = "";
 
-			Binding.Create (() => left == obj.StringValue);
+            Binding.Create (() => left == obj.StringValue);
 
-			Assert.AreEqual (1, obj.PropertyChangedCount);
+            Assert.AreEqual (1, obj.PropertyChangedCount);
 
-			Assert.AreEqual (obj.StringValue, left);
+            Assert.AreEqual (obj.StringValue, left);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual ("Goodbye", left);
-		}
+            Assert.AreEqual ("Goodbye", left);
+        }
 
-		[Test]
-		public void RemoveNotifyPropertyChanged ()
-		{
-			var obj = new NotifyPropertyChangedEventObject {
-				StringValue = "Hello",
-			};
-			var left = "";
+        [Test]
+        public void RemoveNotifyPropertyChanged ()
+        {
+            var obj = new NotifyPropertyChangedEventObject {
+                StringValue = "Hello",
+            };
+            var left = "";
 
-			var b = Binding.Create (() => left == obj.StringValue);
+            var b = Binding.Create (() => left == obj.StringValue);
 
-			Assert.AreEqual (1, obj.PropertyChangedCount);
+            Assert.AreEqual (1, obj.PropertyChangedCount);
 
-			Assert.AreEqual (obj.StringValue, left);
+            Assert.AreEqual (obj.StringValue, left);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual ("Goodbye", left);
+            Assert.AreEqual ("Goodbye", left);
 
-			b.Unbind ();
+            b.Unbind ();
 
-			Assert.AreEqual (0, obj.PropertyChangedCount);
+            Assert.AreEqual (0, obj.PropertyChangedCount);
 
-			obj.StringValue = "Hello Again";
+            obj.StringValue = "Hello Again";
 
-			Assert.AreEqual ("Goodbye", left);
-		}
+            Assert.AreEqual ("Goodbye", left);
+        }
 
-		class NotEventHandlerObject
-		{
-			string stringValue = "";
+        class NotEventHandlerObject
+        {
+            string stringValue = "";
 
-			public int StringValueChangedCount { get; private set; }
+            public int StringValueChangedCount { get; private set; }
 
-			Action<string, string> stringValueChanged; 
-			public event Action<string, string> StringValueChanged 
-			{ 
-				add {
-					stringValueChanged += value;
-					StringValueChangedCount++;
-				} 
-				remove {
-					stringValueChanged -= value;
-					StringValueChangedCount--;
-				}
-			} 
+            Action<string, string> stringValueChanged; 
+            public event Action<string, string> StringValueChanged 
+            { 
+                add {
+                    stringValueChanged += value;
+                    StringValueChangedCount++;
+                } 
+                remove {
+                    stringValueChanged -= value;
+                    StringValueChangedCount--;
+                }
+            } 
 
-			public string StringValue {
-				get { return stringValue; }
-				set {
-					if (stringValue != value) {
-						var oldValue = stringValue;
-						stringValue = value;
-						if (stringValueChanged != null) {
-							stringValueChanged (oldValue, stringValue);
-						}
-					}
-				}
-			}
-		}
+            public string StringValue {
+                get { return stringValue; }
+                set {
+                    if (stringValue != value) {
+                        var oldValue = stringValue;
+                        stringValue = value;
+                        if (stringValueChanged != null) {
+                            stringValueChanged (oldValue, stringValue);
+                        }
+                    }
+                }
+            }
+        }
 
-		[Test]
-		public void NotifyNotEventHandler ()
-		{
-			var obj = new NotEventHandlerObject {
-				StringValue = "Hello",
-			};
-			var left = "";
+        [Test]
+        public void NotifyNotEventHandler ()
+        {
+            var obj = new NotEventHandlerObject {
+                StringValue = "Hello",
+            };
+            var left = "";
 
-			var b = Binding.Create (() => left == obj.StringValue);
+            var b = Binding.Create (() => left == obj.StringValue);
 
-			Assert.AreEqual (1, obj.StringValueChangedCount);
+            Assert.AreEqual (1, obj.StringValueChangedCount);
 
-			Assert.AreEqual (obj.StringValue, left);
+            Assert.AreEqual (obj.StringValue, left);
 
-			obj.StringValue = "Goodbye";
+            obj.StringValue = "Goodbye";
 
-			Assert.AreEqual ("Goodbye", left);
+            Assert.AreEqual ("Goodbye", left);
 
-			b.Unbind ();
+            b.Unbind ();
 
-			Assert.AreEqual (0, obj.StringValueChangedCount);
+            Assert.AreEqual (0, obj.StringValueChangedCount);
 
-			obj.StringValue = "Hello Again";
+            obj.StringValue = "Hello Again";
 
-			Assert.AreEqual ("Goodbye", left);
-		}
-	}
+            Assert.AreEqual ("Goodbye", left);
+        }
+
+        class PlainObject
+        {
+            public string string_property { get; set;}
+        }
+
+        [Test]
+        public void TriggerInvalidateMember()
+        {
+            var obj = new PlainObject {
+                string_property = "Hello",
+            };
+            var left = "";
+
+            var b = Binding.Create (() => left == obj.string_property);
+
+            Assert.AreEqual (obj.string_property, left);
+
+            obj.string_property = "Goodbye";
+
+            Binding.InvalidateMember(() => obj.string_property);
+
+            Assert.AreEqual ("Goodbye", left);
+
+            b.Unbind ();
+
+            obj.string_property = "Hello Again";
+
+            Assert.AreEqual ("Goodbye", left);
+        }
+    }
 }
 
